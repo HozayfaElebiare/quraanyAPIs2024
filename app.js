5021dac702e4c02c637d67d0481992235b13b3ae
Mahmoud Elebiare <33654362+HozayfaElebiare@users.noreply.github.com>
2024-03-12 12:14:46 +0200

    root api

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 
 // Middleware and routes will be added here
 
+
+// add root endpoint / as a simple example
+app.get('/', (req, res) => {
+  res.send('Quraan.me APIs server: Running...');
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
